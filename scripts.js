5b8fff1ffdbdb9be5c814740a85c1dbb5d1b31cb
Rafael Souza Señaris <85324988+rafasenaris@users.noreply.github.com>
2025-03-17 00:53:54 -0300

    Implementação de página de login e salvamento de BD em localStorage

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const bancoUsuarios = [];
+const bancoUsuarios = JSON.parse(localStorage.getItem("bancoUsuarios")) || [];;
 
 function criarUsuario(){
     const usuario = document.getElementById("user").value;
@@ -18,14 +18,36 @@ function cadastrarUsuario(){
 
     if(!novoUser) return;
 
+    const bancoUsuarios = JSON.parse(localStorage.getItem("bancoUsuarios")) || [];
+
     const usuarioEncontrado = bancoUsuarios.find(user => user.usuario === novoUser.usuario);
 
     if(usuarioEncontrado){
         alert("Usuário já cadastrado!");
     }else{
         bancoUsuarios.push(novoUser);
+        localStorage.setItem("bancoUsuarios", JSON.stringify(bancoUsuarios));
         alert(`Usuário ${novoUser.usuario} cadastrado com sucesso!`);
     }
     
+}
+
+function fazerLogin(){
+    const usuarioLogin = document.getElementById("user-login").value;
+    const senhaLogin = document.getElementById("senha-login").value;
+
+    if(!usuarioLogin || !senhaLogin){
+        alert("Usuário ou senha em branco!");
+        return;
+    }
+        
+
+    const usuarioEncontrado = bancoUsuarios.find(user => user.usuario === usuarioLogin && user.senha === senhaLogin);
+
+    if(usuarioEncontrado){
+            alert("sucesso você fez Login");
+    }else{
+            alert("Usuário ou senha incorretos");
+    }
 }
 
